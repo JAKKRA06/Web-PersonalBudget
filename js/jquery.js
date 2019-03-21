8d6ae49b5aeb426790b121375b77b24110ce64a9
Jakub Krajniewski <jakub.krajniewski.programista@gmail.com>
2019-03-21 22:15:29 +0100

    wprowadzono poprawki animacji oraz stylów

--- js/jquery.js
+++ js/jquery.js
@@ -10,17 +10,17 @@ $(function() {
 		});
 
 
-});		
+	});		
 
-$('.custom-select').change(function() {
-		var expense_amount = $('#kwota').val();
-		var expense_category_select = $('.custom-select').val();
+	$('.custom-select').change(function() {
+			var expense_amount = $('#kwota').val();
+			var expense_category_select = $('.custom-select').val();
 
-		var dane = "expense_amount=" + expense_amount + "&expense_category_select=" + expense_category_select;
+			var dane = "expense_amount=" + expense_amount + "&expense_category_select=" + expense_category_select;
 
-		$.post('classes/Ajax.php', dane, function(data) {
-			$('#infoLimit').html(data).hide().fadeIn(800);
+			$.post('classes/Ajax.php', dane, function(data) {
+				$('#infoLimit').html(data).hide().fadeIn(800);
+		});
+
 	});
-
-});
 });
